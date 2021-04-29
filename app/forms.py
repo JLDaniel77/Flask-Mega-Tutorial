486769b3c92a300c9a4aa01908e98cd965d1c977
Jeff <theta_158@yahoo.com>
2021-04-29 13:26:22 -0400

    Adding version control to existing project

---
+++ app/forms.py
@@ -0,0 +1,50 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, PasswordField, BooleanField, 
+                     SubmitField, TextAreaField)
+from wtforms.validators import (InputRequired, ValidationError,
+                                Email, EqualTo, Length)
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(),
+                                                     Length(min=8, max=32)])
+    password2 = PasswordField(
+        'Repeat Password', validators=[InputRequired(), EqualTo('password'),
+                                       Length(min=8, max=32)])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Username already exists')
---
+++ config.py
@@ -0,0 +1,32 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Base config class"""
+    SECRET_KEY = environ.get('SECRET_KEY') or 'Pineapple-on-Pizza'
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_PORT = environ.get('MAIL_PORT')
+    MAIL_USE_TLS = environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    ADMINS = [environ.get('ADMIN_EMAIL')]
+
+
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    FLASK_DEBUG = True
+    FLASK_TESTING = True
+    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI') or \
+        'sqlite:///' + path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
